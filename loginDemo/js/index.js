de756802fdc39076f389753aeb867c6380c49add
bansomin <good091231@126.com>
2017-03-01 12:14:12 +0800

    login

---
+++ loginDemo/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by HAO on 2017/3/1.
+ * Brief   :
+ * Version :
+ */
+
+function clickLoginBtnFunc(){
+	alert("clickLoginBtn");
+}
+
+function clickRegisterBtnFunc(){
+	alert("clickRegisterBtn");
+}
